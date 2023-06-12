b2804eaee70a88e1752d40e42c5463d9e00ac306
jbeckett <jbecktt@umich.edu>
2023-06-12 21:11:44 +0000

    Add comment and clean troubleshooting lines

--- VSI_Incoporation-testing/Step1-Read+Mesh/S1_CopyDisp2Mesh.py
+++ VSI_Incoporation-testing/Step1-Read+Mesh/S1_CopyDisp2Mesh.py
@@ -2,7 +2,6 @@ from ufl import *
 from dolfin import *
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 temp = 0
 filename_disp = 'disp_field_%i'%temp
@@ -10,158 +9,74 @@ filename_specimen = 'specimen_%i'%temp
 
 #meshdir='../mesh/gmsh/holes_5mm_L%ia'%temp
 #meshfilename = '/test%i.xdmf'%temp
-# meshdir='../mesh/gmsh/noholes_L%ia'%temp
-# meshfilename = '/testa.xdmf'
 meshdir = '.'
-meshfilename = '/ShearWavy_6.25MMDisp_Amp_0.6_tet.xdmf' # reading in .xdmf mesh file created in Step1_msh2xdmf.py which describes the mesh stored in the .inp file
-
+meshfilename = '/ShearWavy_6.25MMDisp_Amp_0.6_tet.xdmf' # reading in .xdmf mesh file created in Step1_msh2xdmf.py which describes the original MATLAB .inp mesh in a mesh.io python mesh
 
 # datadir='../result/filtered/20220211-no_holes/raw_data'
 datadir = '.'
-NumStepList = [3]#,2,3]
+TimeStep = [4]#,2,3]
 
-mesh=Mesh() # create empty FEniCS mesh object
+mesh_py=Mesh() # create empty FEniCS mesh object
 
-with XDMFFile(meshdir+meshfilename) as infile: # create 'infile', an XDMFFile object imported from the file named in 'meshfilename'
-    infile.read(mesh) # read in data from .xdmf file to mesh object called infile (used to access geometry/connectivity info about the mesh)
-    
+with XDMFFile(meshdir+meshfilename) as infile: # create 'infile', an XDMFFile object imported from 'meshfilename'
+    infile.read(mesh_py) # read in data from .xdmf file to mesh object called infile (used to access geometry/connectivity info about the mesh) 
 
-V = VectorFunctionSpace(mesh, "CG", 1) # create vector function space for 'mesh' composed of linear piece-wise continuous Lagrangian functions
-W = FunctionSpace(mesh, "Lagrange", 1) # Create a scalar function space for 'mesh'composed of linear piece-wise continuous Lagrangian functions
+V = VectorFunctionSpace(mesh_py, "CG", 1) # create vector function space for 'mesh' composed of linear piece-wise continuous Lagrangian functions
+W = FunctionSpace(mesh_py, "Lagrange", 1) # Create a scalar function space for 'mesh'composed of linear piece-wise continuous Lagrangian functions
 
 # Define functions
 u = Function(V) 
 
-coordinate_elem_TXTdata=pd.read_csv(datadir + '/nod_u_GOOD.txt', header=None).to_numpy()[:,0:3] # make coordinate_data variable which contains node positions from node_pos.txt ()
-# print(coordinate_data.shape)
-# print("FROM THE MESH")
-# print(mesh.coordinates()[:]) # This may very well be the issue
-# print(mesh.coordinates()[:].shape)
-# print("FROM SUS TXT FILE")
-# print(coordinate_elem_TXTdata)
-# print(coordinate_elem_TXTdata.shape)
+coordinate_elem_ABAQUS=pd.read_csv(datadir + '/nod_u_GOOD.txt', header=None).to_numpy()[:,0:3] # make coordinate_data variable which contains node positions from ABAQUS .txt data 
+displacement_elem_ABAQUS = [0]*len(TimeStep) # initialize 'displacement_data' as a zeros vector of dimension 1 by len(NumStepList) (currently yields a 1 by 1).
 
+for count,tid in enumerate(TimeStep):
+	displacement_elem_ABAQUS[count]=pd.read_csv(datadir + '/disp_'+str(tid)+'.txt', header=None).to_numpy()[:,0:3]
 
-displacement_elem_TXTdata = [0]*len(NumStepList) # initialize 'displacement_data' as a zeros vector of dimension 1 by len(NumStepList) (currently yields a 1 by 1). should this have the same dimensions as coordinate_data.shape?
+DOF2VertexMap = pd.read_csv('dof2vertex.txt', header=None).to_numpy()[:,0:3]
 
-for count,tid in enumerate(NumStepList):
-	displacement_elem_TXTdata[count]=pd.read_csv(datadir + '/disp_'+str(tid)+'.txt', header=None).to_numpy()[:,0:3]
-      
-print(type(displacement_elem_TXTdata))
-testing123 = np.concatenate(displacement_elem_TXTdata, axis=0)
-first_array = np.array(displacement_elem_TXTdata[0])  # Select the first array
-if isinstance(first_array,np.ndarray):
-  print("is array!! woot woot oh yeah")
-np.savetxt("nodeTXT.txt", coordinate_elem_TXTdata, delimiter=',', fmt='%f')    
-np.savetxt("dispTXT.txt", testing123, delimiter=',', fmt='%f')
-# print(count)
-# print(tid)
-# print(displacement_TXTdata)
+coordinates_nodes = mesh_py.coordinates()
 
-
-# print(dof_to_vertex_map(V))
-# print(dof_to_vertex_map(W))
-# d2v_vector = dof_to_vertex_map(V) # Sid is going to try to find
-# a = dof_to_vertex_map(V)
-a = pd.read_csv('dof2vertex.txt', header=None).to_numpy()[:,0:3]
-
-# random_Class = V.dofmap()
-# print(dir(random_Class))
-# a = V.dofmap().tabulate_local_to_global_dofs() # currently this is just print an array going from 0 to 3*NumberOfNodes in increments of 1 (in order) (i.e., simple mapping between local and global DOF)
-# print(mesh)
-# print(a[198065])
-# print(len(a))
-#print(a.shape)
-# exit()
-coordinates_nodes = mesh.coordinates()
-
-
-
-
-
-# print(coordinates_nodes[0])
-# print(coordinates_nodes[1])
-# exit()
-
-num_elements_data,_= coordinate_elem_TXTdata.shape
+num_elements_data,_= coordinate_elem_ABAQUS.shape
 num_nodes_data,_=coordinates_nodes.shape
 print('num_elements =',num_elements_data)
 print('num_nodes =',num_nodes_data)
 
-# num_displacement_file,_=displacement_data.shape
-# print('num_displacement in file =',num_displacement_file)
-
-u_array=np.zeros((num_nodes_data*3, len(NumStepList))) # create array (1 x num_coordinate in mesh) of 0's to preallocate
-test_disp = np.zeros((num_nodes_data, len(NumStepList)))
-test_index = np.zeros((num_nodes_data, len(NumStepList)))
-test_count = np.zeros((num_nodes_data, len(NumStepList)))
-for i in range(num_nodes_data): # looping over all nodes in data and finding nearest element
+u_array=np.zeros((num_nodes_data*3, len(TimeStep))) # create array (1 x num_coordinate in mesh) of 0's to preallocate
+for i in range(num_nodes_data): # looping over all nodes in data 
+  # Find nearest MATLAB node for each Python node
   X=coordinates_nodes[i] # import coordinates for node i in 'mesh'
   index=-1
-  # print(coordinate_data[:,:].shape)
-  # test = X*np.ones([num_elements_data, 1])
-  # print(test.shape)
-  # print(X.shape)
-  _dist = coordinate_elem_TXTdata[:,:]-X*np.ones([num_elements_data, 1]) 
+  _dist = coordinate_elem_ABAQUS[:,:]-X*np.ones([num_elements_data, 1]) 
   _dist = np.linalg.norm(_dist,axis = 1) # calculating distance from X[i] to each node 
   index = np.argmin(_dist) # index of element located closest to the ith node
-  test_disp[i] = np.min(_dist)
-  test_index[i] = index
-  # print(f'loop: {i}, index: {index}\n')
-  # print(index)
-  # print(index)
-  # print(_dist[index])
-  # print('i=',i,' index=',index,' X=',X,' coordinate_data=',coordinate_data[index,:])
-  for count,tid in enumerate(NumStepList):
-  #  print(index)
-  #  print(coordinate_elem_TXTdata[index,:])
-  #  print(displacement_elem_TXTdata[count][index,:],(-1))
-  #  input("next")
-    test_count[i] = count
-    #pos_i = np.reshape(X,(-1))
-    #u_array[i*3:i*3+3, count] = pos_i
-    u_array[i*3:i*3+3, count] = np.reshape(displacement_elem_TXTdata[count][index,:],(-1))
-  #  print(u_array[0:3])
-   # print(u_array.shape)
-   # print(u_array[i*3:i*3+3, count][:])
-    #u_array[i*3:i*3+3, count]=np.reshape([1,2,3],(-1))
+  
+  # Write displacement data from nearest node to Python node
+  for count,tid in enumerate(TimeStep):
+    u_array[i*3:i*3+3, count] = np.reshape(displacement_elem_ABAQUS[count][index,:],(-1))
 
-plt.hist(test_index, bins=60)
-plt.show()
-plt.savefig('histogramINDEX.png')
-
-print("test_count")
-print(sum(test_count))
-
-# print("test")
-# print(u_array[0:6])
-# print(u_array.shape)
-
-# checking u
-check_disp = u.compute_vertex_values(mesh)
-check_loc = mesh.coordinates()
+# checking u in MATLAB
+check_disp = u.compute_vertex_values(mesh_py)
+check_loc = mesh_py.coordinates()
 np.savetxt("disp_u.txt", check_disp, delimiter=',', fmt='%f')    
 np.savetxt("nod_u.txt", check_loc, delimiter=',', fmt='%f')
 
-
-
-file_1 = XDMFFile(mesh.mpi_comm(),datadir + '/'+filename_disp+'.xdmf')
+# Write .xdmf/.h5 files with 
+file_1 = XDMFFile(mesh_py.mpi_comm(),datadir + '/'+filename_disp+'.xdmf')
 file_2 = HDF5File(MPI.comm_world, datadir + '/'+filename_specimen+'.h5', 'w')
 file_2.write(u,'/mesh')
 
-
-for count,tid in enumerate(NumStepList):
-  u.vector().set_local(u_array[a, count])
+for count,tid in enumerate(TimeStep):
+  # Adding displacements to 'u'
+  u.vector().set_local(u_array[DOF2VertexMap, count])
   u.vector().apply("insert")
 
-
-  # Save solution 
+  # Save solution to .xdmf/.h5
   file_1.write(u, tid)
-  # #
   file_2.write(u,'/disp%i'%tid)
 
+# Check u
 print(u)
-
 
 file_1.close()
 file_2.close()
--- VSI_Incoporation-testing/Step1-Read+Mesh/Step1_msh2xdmf.py
+++ VSI_Incoporation-testing/Step1-Read+Mesh/Step1_msh2xdmf.py
@@ -1,6 +1,4 @@
 import meshio
-#from dolfin import Mesh,MeshFunction,MeshEditor,Point,Measure,assemble
-#from dolfin import XDMFFile, HDF5File, MPI, dx, Constant
 from dolfin import *
 # from dolfin_adjoint import *
 import os
@@ -27,47 +25,32 @@ loadPath = "./"
 savePath = "."
 filename = "ShearWavy_6.25MMDisp_Amp_0.6_tet"
 
+# Convert MATLAB mesh (.inp) to a python mesh (.xdmf/.h5)
 def msh2xdmf(loadPath,savePath,filename):
 
-    msh = meshio.read(os.path.join(loadPath, filename + '.inp')) # read .inp mesh file from MATLAB and create meshio.Mesh object to store information describing the mesh
-    # print(msh.points)
-    # print(msh.cells[0].data)
-    # exit()
+    msh_matlab = meshio.read(os.path.join(loadPath, filename + '.inp')) # read .inp mesh file from MATLAB and create meshio.Mesh object to store information describing the mesh
 
     #Find mesh size params
-    num_vertex = msh.points.shape[0] 
-    num_cell = msh.cells[0].data.shape[0]
-    # print(num_vertex)
-    # print(num_cell)
+    num_vertex = msh_matlab.points.shape[0] 
+    num_cell = msh_matlab.cells[0].data.shape[0]
 
     #Initialize FEniCS mesh 
-    mesh = Mesh() # create empty dofin.Mesh object 
+    mesh_py = Mesh() # create empty dofin.Mesh object 
 
     editor = MeshEditor() # iniate MeshEditor object
-    editor.open(mesh, type="tetrahedron",tdim=3, gdim=3) #"hexahedron" # define tetrahedron mesh with 3 topologic and geometric dimensions
+    editor.open(mesh_py, type="tetrahedron",tdim=3, gdim=3) # define tetrahedron (or "hexahedron") mesh with 3 topologic and geometric dimensions
     editor.init_vertices(num_vertex) # defining verticies
     editor.init_cells(num_cell) # initialize cells
-    #HexCellsDataID = msh.cell_data['gmsh:physical'][0]
 
     for i in range(num_vertex):
-        editor.add_vertex(i, Point(msh.points[i,:])) # defining coordinates of ith vertex
-        # print(msh.points[i,:].shape)
-        # print(msh.points)
-        # print(msh.points.shape)
+        editor.add_vertex(i, Point(msh_matlab.points[i,:])) # defining coordinates of ith vertex
     for i in range(num_cell):
-        try:
-            #_cell_gmsh = msh.cells[0].data[i,:]
-            #_cell_vtk = gmsh2vtk_hex(_cell_gmsh)
-            #_cell_vtk = msh.cells[0].data[i,:]
-            #_cell_dolfin = vtk2dolfin_hex(_cell_vtk)			
-            _cell_dolfin = ufl_simplicial_order(msh.cells[0].data[i,:]) # defining verticies of the ith cell in ascending order 
-            # print(msh.cells[0].data)
-            # print("test")
-            # print(_cell_dolfin)
+        try:		
+            _cell_dolfin = ufl_simplicial_order(msh_matlab.cells[0].data[i,:]) # defining verticies of the ith cell in ascending order 
             editor.add_cell(i,_cell_dolfin) # add ith cell to MeshEditor object 
         except RuntimeError:
             print("Error in cell index %i"%i)
-            print(msh.cells[0].data[i,:4])
+            print(msh_matlab.cells[0].data[i,:4])
             raise
     try: 
         print('==============================')
@@ -77,65 +60,34 @@ def msh2xdmf(loadPath,savePath,filename):
         print("Error in closing the editor")
         print(err)
         pass	
-
     try:
         print('==============================')
-        mesh.order() # Finding the highest order of basis function used in the mesh
-        print('Mesh ordered: ', mesh.ordered()) # checking that the mesh is well-defined in terms of the ordering of its verticies and cells
-        # Subdomains will be the cells 
-        # subdomains = MeshFunction('size_t',mesh,mesh.topology().dim())
-        #mvc = MeshValueCollection('size_t',mesh,mesh.topology().dim())
-        #subdomains.array()[:] = HexCellsDataID
+        mesh_py.order() # Finding the highest order of basis function used in the mesh
+        print('Mesh ordered: ', mesh_py.ordered()) # checking that the mesh is well-defined in terms of the ordering of its verticies and cells
     except RuntimeError:
         print("Error ordering the mesh and generating mesh function")
         raise
-
-    #Write element id as a mesh function
-    # write/export cellfunction
-
+    
+    # Saving python mesh to .xdmf file
     try:
         print('==============================')
-        # with XDMFFile(MPI.comm_world,savePath+'/'+filename+'.xdmf') as file:
         with XDMFFile(MPI.comm_world,os.path.join(savePath,filename+'.xdmf')) as file:
-            file.write(mesh)
-            # file.write(subdomains)
-        # with HDF5File(MPI.comm_world,savePath+'/'+filename+'_ID.h5','w') as file:
-        # with HDF5File(MPI.comm_world,os.path.join(savePath,filename+'_ID.h5'),'w') as file:
-        #     file.write(mesh,'/mesh')
-        #     file.write(subdomains,'/meshfunction')
-        #     #file.write(mf,"my_mf")
-        #     #file_IndexData = XDMFFile(MPI.comm_world,filename+'_ID.xdmf');
-        #     #file_IndexData.write(mf)
-            #XDMFFile('mesh.xdmf').write(mesh)
+            file.write(mesh_py)
         print("Mesh saved")
     except RuntimeError:
         print("Error in saving file")
         raise
 
-    #Test assembly
-    # ds_custom1 = Measure("ds", domain=mesh, subdomain_data=subdomains, subdomain_id=1)
-    # ds_custom2 = Measure("ds", domain=mesh, subdomain_data=subdomains, subdomain_id=2)
-    # ds_custom3 = Measure("ds", domain=mesh, subdomain_data=subdomains, subdomain_id=3)
-    # print(assemble(1*ds_custom1))
-    # print(assemble(1*ds_custom2))
-    # print(assemble(1*ds_custom3))
-    # print(assemble(Constant(1)*dx))
-
-    #Now try opening the file
-    mesh = Mesh()
+    # Now try opening the file
+    mesh_py = Mesh()
 
     try:
         print('==============================')
         with XDMFFile(MPI.comm_world,filename+'.xdmf') as file:
-            file.read(mesh)
-            V = VectorFunctionSpace(mesh, "CG", 1)
+            file.read(mesh_py)
+            V = VectorFunctionSpace(mesh_py, "CG", 1)
             a = dof_to_vertex_map(V)
-            np.savetxt('dof2vertex.txt',a, fmt='%i')
-            # mvc = MeshValueCollection('size_t',mesh2,mesh2.topology().dim())
-            # file.read(mvc,'f')
-            # mf = cpp.mesh.MeshFunctionSizet(mesh, mvc)
-            # print(mf.array()[:])
-            # #mf2 = MeshFunction('size_t',mesh2,mesh2.topology().dim())
+            # np.savetxt('dof2vertex.txt',a, fmt='%i') # check DOF to vertex map
             print('Mesh is readable')	
     except RuntimeError as err:
         print("Error in reading the file")
