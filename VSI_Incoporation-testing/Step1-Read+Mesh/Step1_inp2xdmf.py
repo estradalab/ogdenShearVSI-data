cff59963a6ae493f0dfe0b8c8ba34671c528cc27
jbeckett <jbecktt@umich.edu>
2023-06-22 12:50:10 +0000

    Save only linear tet nodes to new mesh
    Previously all of the nodes from the original mesh were saved to the linear tet mesh files. Although the quadratic nodes should not have been referenced because they were removed from the connectivity matrix, they were removed to speed up the code and to ensure that the code functions properly. The files still display correctly and various other checks were conducted to ensure the files were not corrupted as a results of the change

--- VSI_Incoporation-testing/Step1-Read+Mesh/Step1_inp2xdmf.py
+++ VSI_Incoporation-testing/Step1-Read+Mesh/Step1_inp2xdmf.py
@@ -1,4 +1,4 @@
-# Overview: read mesh defined by <filename>.inp and write mesh to <filename>.xdmf/.h5
+# Overview: read quad tet mesh defined by <filename>.inp and write simplified linear tet mesh to <filename>.xdmf/.h5
 
 import meshio
 from dolfin import *
@@ -21,43 +21,47 @@ def vtk2gmsh_hex(P):
 def ufl_simplicial_order(P):
     return np.sort(np.array(P), axis=None).tolist()	 # sorts vector P in ascending order
 
-# loadPath = "./InputFiles/22-1215-Wavy_Sweep_v2/"
 loadPath = "./"  
 savePath = "."
 filename = "sq-8mm_sin-per-4_sin-amp-2mm_tet"
 
 
-# Convert Abaqus/MATLAB quad tet mesh (.inp) to a linear tet python mesh (.xdmf/.h5)
+# Convert quad tet mesh (.inp) to a linear tet python mesh (.xdmf/.h5)
 def inp2xdmf(loadPath,savePath,filename):
 
-    mesh_matlab = meshio.read(os.path.join(loadPath, filename + '.inp')) # read .inp mesh file from MATLAB and create meshio.Mesh object to store information describing the mesh
+    # Write quad tet data to meshio.Mesh object
+    mesh_matlab = meshio.read(os.path.join(loadPath, filename + '.inp'))
 
-    #Find MATLAB mesh size params
+    # Find quad tet mesh size params
     num_vertex = mesh_matlab.points.shape[0] 
     num_cell = mesh_matlab.cells[0].data.shape[0]
 
-    #Initialize FEniCS mesh 
+    # Initialize FEniCS mesh 
     mesh_py = Mesh() # create empty dofin.Mesh object 
     editor = MeshEditor() # iniate MeshEditor object
     editor.open(mesh_py, type="tetrahedron",tdim=3, gdim=3) # define tetrahedron (or "hexahedron") mesh with 3 topologic and geometric dimensions
-    editor.init_vertices(num_vertex) # defining verticies in python mesh to be the same as MATLAB
-    editor.init_cells(num_cell) # initialize cells in python mesh to be the same as MATLAB
+    editor.init_vertices(np.max(mesh_matlab.cells[0].data[:,:4])+1) # initialize cells in dolfin mesh - the number of verticies is equal to the number of linear nodes on the quad tet mesh 
+    editor.init_cells(num_cell) # initialize cells in python mesh - the number of elements is equal to the quad tet mesh
 
-    for i in range(num_vertex):
-        editor.add_vertex(i, Point(mesh_matlab.points[i,:])) # defining coordinates of ith vertex
+    # for i in range(num_vertex):
+    for i in range(np.max(mesh_matlab.cells[0].data[:,:4])+1): 
+        editor.add_vertex(i, Point(mesh_matlab.points[i,:])) # defining coordinates of ith vertex from the nodes included on the linear portion of the quadratic tets
     for i in range(num_cell):
         try:		
             _cell_dolfin = ufl_simplicial_order(mesh_matlab.cells[0].data[i,:]) # defining verticies of the ith cell in ascending order 
-            # print(_cell_dolfin[0:4])
-            # exit()
             editor.add_cell(i,_cell_dolfin[0:4]) # add first 4 nodes (linear portion of tet) to ith connectivity matrix  to MeshEditor object. Effectively converts quad --> linear tet mesh.
         except RuntimeError:
             print("Error in cell index %i"%i)
             print(mesh_matlab.cells[0].data[i,:4])
             raise
+    # Call out relevant information about the transitioin to linear tets from quadratic tets
+    print("linear tet uses nodes between",np.min(mesh_matlab.cells[0].data[:,:4]),"to",np.max(mesh_matlab.cells[0].data[:,:4])) 
+    print("quad tet uses nodes between",np.min(mesh_matlab.cells[0].data[:,:]),"to",np.max(mesh_matlab.cells[0].data[:,:]))
+    print("quad tet uses linear nodes and",np.min(mesh_matlab.cells[0].data[:,4:]),"to",np.max(mesh_matlab.cells[0].data[:,4:]))
+    print("num_vertex variable is equal to",num_vertex)
     try: 
         print('==============================')
-        editor.close()
+        editor.close() # close MeshEditor object
         print("Editor closed")
     except RuntimeError as err:
         print("Error in closing the editor")
@@ -70,7 +74,7 @@ def inp2xdmf(loadPath,savePath,filename):
     except RuntimeError:
         print("Error ordering the mesh and generating mesh function")
         raise
-    # Saving python mesh to .xdmf file
+    # Save python mesh to .xdmf/.h5 files
     try:
         print('==============================')
         with XDMFFile(MPI.comm_world,os.path.join(savePath,filename+'.xdmf')) as file:
@@ -80,7 +84,7 @@ def inp2xdmf(loadPath,savePath,filename):
         print("Error in saving file")
         raise
 
-    # Now try opening the file
+    # Attempt to open mesh
     mesh_py = Mesh()
 
     # Final check on .xdmf form of Python mesh
@@ -90,7 +94,7 @@ def inp2xdmf(loadPath,savePath,filename):
             file.read(mesh_py)
             V = VectorFunctionSpace(mesh_py, "CG", 1)
             a = dof_to_vertex_map(V)
-            np.savetxt('dof2vertex.txt',a, fmt='%i') # save DOF to vertex map for S1_copyDisp2Mesh.py
+            np.savetxt('dof2vertex.txt',a, fmt='%i') # save DOF to vertex map for linear tet mesh
             print('Mesh is readable')	
     except RuntimeError as err:
         print("Error in reading the file")
--- VSI_Incoporation-testing/Step1-Read+Mesh/Step2_addU2xdmf.py
+++ VSI_Incoporation-testing/Step1-Read+Mesh/Step2_addU2xdmf.py
@@ -4,7 +4,6 @@ from dolfin import *
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.graph_objects as go
 
 temp = 0
 filename_disp = 'disp_field_%i'%temp
@@ -14,19 +13,19 @@ meshdir = '.'
 meshfilename = '/sq-8mm_sin-per-4_sin-amp-2mm_tet.xdmf' # reading in .xdmf mesh file created in Step1_msh2xdmf.py which describes the original MATLAB .inp mesh in a mesh.io python mesh
 
 datadir = '.'
-TimeStep = [2]
+TimeStep = [1]
 
 mesh_py=Mesh() # create empty FEniCS mesh object
 
 with XDMFFile(meshdir+meshfilename) as infile: # create 'infile', an XDMFFile object imported from 'meshfilename'
-    infile.read(mesh_py) # read in data from .xdmf file to mesh object called infile (used to access geometry/connectivity info about the mesh) 
+  infile.read(mesh_py) # read in data from .xdmf file to mesh object called infile (used to access geometry/connectivity info about the mesh) 
 
 V = VectorFunctionSpace(mesh_py, "CG", 1) # create vector function space for 'mesh' composed of linear piece-wise continuous Lagrangian functions
 W = FunctionSpace(mesh_py, "Lagrange", 1) # Create a scalar function space for 'mesh'composed of linear piece-wise continuous Lagrangian functions
 
 u = Function(V) # initialize function object
 
-X_Abaqus=pd.read_csv(datadir + '/X_061623_validationtest2.csv', header=None).to_numpy()[:,0:3] # make coordinate_data variable which contains node positions from ABAQUS .txt data
+X_Abaqus=pd.read_csv(datadir + '/X_062123_crop.csv', header=None).to_numpy()[:,0:3] # make coordinate_data variable which contains node positions from ABAQUS .txt data
 displacement_elem_Abaqus = [0]*len(TimeStep) # initialize 'displacement_data' as a zeros vector of dimension 1 by len(NumStepList) (currently yields a 1 by 1).
 
 # Check geometries are the same between Abaqus output and mesh object from .inp file in Step1
@@ -54,7 +53,7 @@ print("z lims [",np.min(X_mesh[:,2]),np.max(X_mesh[:,2]),"]")
 
 for count,tid in enumerate(TimeStep):
 	# displacement_elem_ABAQUS[count]=pd.read_csv(datadir + '/disp_'+str(tid)+'.txt', header=None).to_numpy()[:,0:3]
-  displacement_elem_Abaqus[count]=pd.read_csv(datadir + '/U_061623_validationtest_2.csv', header=None).to_numpy()[:,0:3]
+  displacement_elem_Abaqus[count]=pd.read_csv(datadir + '/U_062123_crop.csv', header=None).to_numpy()[:,0:3]
 
 DOF2VertexMap = pd.read_csv('dof2vertex.txt', header=None).to_numpy()[:,0:3] # Read in dof2vertex.txt map
 
@@ -62,11 +61,10 @@ X_node_mesh = mesh_py.coordinates()
 
 num_nodes_csv,_= X_Abaqus.shape
 num_nodes_mesh,_=X_node_mesh.shape
-# print('num_nodes_csv =',num_nodes_csv)
-# print('num_nodes_inp =',num_nodes_mesh)
 
 u_array=np.zeros((num_nodes_mesh*3, len(TimeStep))) # create array (1 x num_coordinate in mesh) of 0's to preallocate
 index_test = np.zeros(num_nodes_mesh)
+
 if X_Abaqus.shape == X_node_mesh.shape:
   print("Abaqus output and 'mesh' have the same number of nodes")
   if np.max(np.linalg.norm(X_Abaqus-X_node_mesh))/X_Abaqus.shape[0] > 0.0001: # interpolate if nodes are far from each other
